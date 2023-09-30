c19843f988329fa74fc66d0614b0c42a3df40f9a
cakuang1 <cakuang1@berkeley.edu>
2023-09-30 16:28:29 -0700

    fixed

--- frontend/pages/projects/pipeline.tsx
+++ frontend/pages/projects/pipeline.tsx
@@ -5,82 +5,137 @@ import MarkdownComponent from "@/components/md"
 export default function Stock() {
 
 
-  const md = `# stocksimulator
+  const md = `## DoorDash Snowflake pipeline
 
-
-  A quick and easy web application to build your stock portfolio. Search for your favorite stocks and add them to your portfolio through a simple UI. The application will track your portfolio through your browsers storage and allow real time updates to determine how your portfolio is doing using external APIs.
+  A manual step-by-step data pipeline that extracts data files from s3, transforms the data, and loads them into a snowflake table. 
+  
+  
+  
+  ## Pipeline Flow
+  ![alt text](pipepic.svg)
   
   
   
   
-  ## FrontEnd Endpoints
   
-  Method	| Path	| Description	|
-  ------------- | ------------------------- | ------------- |
-  GET	| /	| Index page. View your portfolio here| 	
-  GET	| /stocks	| Trending stocks page and Stock search feature|
-  GET	| /stocks/str:ticker/	| Ticker data of a specific stock. Includes Graph and common markers	|
-  GET | /trade/str:ticker/| Buy screen for a ticker|
-  GET	| /str:purchaseid/	| Register new account	|
+  ## Dataset Description
+  
+  We are dealing with DoorDash transactional data. Each row represents a doordash order.
   
   
-  ## BackEnd REST Endpoints
-  Method	| Path	| Description	|
-  ------------- | ------------------------- | ------------- |
-  GET	| /trending	| Get trending tickers and their data| 	
-  GET	| /ticker/str:ticker/| Get specified ticker data	|
-  GET	| search/str:query/	| Grab search query results from the given query	|
-  GET	| portfolio/str:tickers/| Grab ticker data for the stocks in your portfolio	|
+  | Field Name      | Description |
+  | ----------- | ----------- |
+  | Customer_placed_order_datetime      | The data and time when the customer placed the order |
+  | Driver_at_restaurant_datetime  | The date and time when the driver arrived at the restaurant|
+  | Delivered_to_consumer_datetime   | The date and time when the food was delivered to the consumer    |
+  | Driver_ID   | The ID of the Driver      |
+  | Restaurant_ID  | The ID of the Restaurant      |
+  | Consumer_ID  | The ID of the Consumer    |
+  | Is_New  |  Whether or not the consumer is a new user |
+  | Delivery_Region  | Region of the customer address |
+  | Is_ASAP | Customer included|
+  | Order_total | The total Order amount  |
+  | Amount_of_discount | Discounted amount towards the Order | 
+  | Amount_of_tip| Total amount of tips the customer gave|
+  | Refunded_amount | The amount refunded  | 
+  | Total_time_elapsed | Total time it took for the order to be delivered after the customer placed the order | 
   
   
-  ## Running Locally
   
-  If you wanted to test this application out locally, you can run the developement servers locally using docker.
+  ## S3 description
   
+  ### The bucket contains 3 directories
   
-  1. Clone the repository locally
-     \`\`\`bash
-     git clone https://github.com/cakuang1/stocksimulator/
-     cd stocksimulator/
-     \`\`\`
-  
-  2. Install the required python packages
-     \`\`\`bash
-     cd clickschrome
-     pip install -r producer/requirements.txt
-     pip install -r consumer/requirements.txt
-     \`\`\`
-
-
-  3. Browser:
-      The frontend and backend containers should now be running on ports 3000 and 8000 respectively.
-       Open a web browser and navigate to http://localhost:3000. The application should working.
-  4. Stop Containers
-      When finished, stop the containers by either running docker-compose down, or manually stopping the containers on docker desktop.
-  
-  ## Demo
+  | Directory    | Description |
+  | ----------- | ----------- |
+  | raw    | The initial directory that was created in our setup script. Contains all the raw files. For convienience, the filenames are simply  |
+  | clean | Contains cleaned files |
+  | feateng | The date and time when the driver arrived at the restaurant|
   
   
-  ![Alt Text](/stock.gif)
   
-  ## Created Using
   
-  - React (Front End)
-  - Django (Back End) 
-  - Yahoo Query (External API/Webscraper)
-  - ApeWisom (External API for trending list)
-  - Misc
-      - Pandas (Working with yahoo queries historical data)
-      - Djanjo REST Framework
-      - HeroIcons (for free icons)
-  ## How this works
-  - -Django works as a proxy server in order to call the external API. There is no database involved. 
-  - -YahooQuery feeds data to the backend (Simply a webscraper that scrapes Yahoo Finannce)
-  - -Django transforms this data and sends it to the client to allow for real-time stock information
-  ## Features
-  - -Live Search - Search for your favorite stocks through the live search bar.
-  - -Live Stock Information - Stock Information display (Graph,Live Prices)
-  - -Portfolio Tracking - Allows for your purchased stocks to be tracked through your browsers local storage and added on 
+  ## Pipe-Line Components (/etl)
+  
+  | script   | purpose | how to run |
+  | ----------- | ----------- |
+  | cleaning.py     | pulls the file from the raw directory, cleanses it, and saves the copy to clean| |
+  | fe.py  pulls the file from the clean directory, adds needed fileds, and saves the copy to feateng| |
+  | push.py  | imports file and validates data. If data is validated, push into warehouse| |
+  
+  
+  ## Getting Started
+  
+  ### Requirements
+      -AWS account (IAM user with full access to S3). You need:
+          -AWS_ACCESS_KEY_ID
+          -AWS_SECRET_ACCESS_KEY
+      -SnowFlake account(Used the free trial). You need:
+          -Your account username
+          -Your account password
+          -Your account name
+      -Docker Installed on local machine
+  ### Setup
+  
+  1. Create and env file named .env with the content below. Fill in the # with the your snowflake information and AWS keys.
+  
+  \`\`\`
+  snowflake_user = '######'
+  snowflake_password = '######'
+  snowflake_account = '######'
+  
+  snowflake_database = 'ddtosfpipeline' 
+  snowflake_schema = 'ddtosfpipelineschema' 
+  AWS_REGION = "us-west-1" 
+  BUCKET_NAME = 'ddsfpipeline' 
+  
+  
+  \`\`\`
+  
+  2. Build your docker image using the command. This sets up the environment.
+  
+  \`\`\`
+  docker build -t myimage:latest 
+  \`\`\`
+  
+  3. Using the new container, you are able to run the scripts in the containerized command line. Now you can run the setup file using this command line.
+  
+  \`\`\`
+  python3 setup.py
+  \`\`\`
+  
+  
+  
+  ### Running the pipeline 
+  Using the same containerized command line, you can now run the 3 scripts in order.
+  
+  \`\`\`
+  python3 /etl/cleaning.py
+  python3 /etl/fe.py
+  python3 /etl/push.py
+  \`\`\`
+  
+  ## Data Source
+  
+  
+  <b>NOTE</b> I believe this dataset is purely synthetic. I am simply using the data set as a way to work with transactional data. Since I am creating this project for learning purposes, the actual data does not really matter. (Any dataset could work)
+  
+  
+  
+  <b> Tools used:</b>
+  - Python
+  - Data lake: [AWS S3](https://aws.amazon.com/s3/)
+  - Data transformation: [Pandas](https://www.getdbt.com/)
+  - Data warehouse: [Snowflake](https://cloud.google.com/bigquery)
+  - Containerization : [Docker](https://www.docker.com/)
+  <b>
+  
+  
+  ## Future work
+  
+  -Automating the pipeline using a cron job or using an Orchestration tool such as airflow
+  
+  
   
   
   `
