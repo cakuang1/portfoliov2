64d875134f4df2fb70997f91d00b14b8fbdf82ee
cakuang1 <cakuang1@berkeley.edu>
2023-11-20 00:01:02 -0800

    fixed bad component

--- frontend/pages/blogs/index.tsx
+++ frontend/pages/blogs/index.tsx
@@ -1,7 +1,4 @@
 import Layout from "@/components/layout"
-import Link from "next/link"
-
-
 
 export default function Blog() {
   return (
@@ -10,6 +7,7 @@ export default function Blog() {
           <div className="text-center mt-14">
                 <h1 className="font-bold text-3xl inline-block border-b-2 border-purple  ">Blog</h1>
                 <h1 className="text-2xl text-center mt-8"> I enjoy reading and learning new things, so I write these 'blogs' as a way of retaining and reviewing what I have learned.</h1>
+                    
                 <div>
                     
                 </div>
